1866f19208c5ccc8c966d4dcc268c979dcc14230
Zachary Hao <zacharyhao@hotmail.com>
2019-11-13 22:18:44 +0800

    initial

---
+++ hello world.py
@@ -0,0 +1,9 @@
+import numpy as np
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import re
+test='abc/def'
+a=test.split('/')[0]
+print (a)
